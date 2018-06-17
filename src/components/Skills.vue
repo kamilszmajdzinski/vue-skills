155b9d14d994c5b73edcf07b6cbe2220dc95d7c1
kamilszmajdzinski <kamil.szmajdzinski@gmail.com>
2018-06-17 23:08:14 +0200

    add styles

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,29 +1,58 @@
 <template>
   <div class="hello">
     <h1>Skills</h1>
+    <div class="holder">
+      <ul>
+        <li v-for = "(data, index) in skills" :key="index" > {{ data.skill }} </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Skills',
+  data(){
+    return{
+      skills: [
+        {"skill": "react.js"},
+        {"skill": "redux"},
+        {"skill": "react-router"},
+      ]
+    }
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  .holder {
+    background: #fff;
+  }
+
+  ul {
+    margin: 0;
+    padding: 0;
+    list-style-type: none;
+  }
+  
+  ul li {
+    padding: 20px;
+    font-size: 1.3em;
+    background-color: #E0EDF4;
+    border-left: 5px solid #3EB3F6;
+    margin-bottom: 2px;
+    color: #3E5252;
+  }
+
+  p {
+    text-align:center;
+    padding: 30px 0;
+    color: gray;
+  }
+
+  .container {
+    box-shadow: 0px 0px 40px lightgray;
+  }
+
 </style>
