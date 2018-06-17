822948d1dba0801b04b8d590d0b5bfeaa543a6ce
kamilszmajdzinski <kamil.szmajdzinski@gmail.com>
2018-06-17 13:27:13 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <Skills msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import Skills from './components/Skills.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Skills
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
